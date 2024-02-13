285a6150d1b14323781093c87369306c89168608
Almudena Rivera <riveraferalmudena@gmail.com>
2024-02-13 10:23:17 +0100

    Componentes show y create okey

--- reactfront/src/App.js
+++ reactfront/src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 
+//Importamos componentes
+import CompShowBlogs from './blog/ShowBlog';
+import CompCreateBlog from './blog/CreateBlog';
+
+//importar router
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <BrowserRouter>
+        <Routes>
+          {/* Componentes */}
+        <Route path='/' element={ <CompShowBlogs />} />
+        <Route path='/create' element={ <CompCreateBlog />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ reactfront/src/blog/EditBlog.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const URI = 'http://localhost:8000/blogs/'
